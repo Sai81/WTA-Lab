468ad9585e7bc090276365d421b100a882e26e2a
Sai40481 <saisrivatsa81@gmail.com>
2021-02-13 22:12:31 +0530

    updated product-demo.js

--- product-demo.js
+++ product-demo.js
@@ -26,3 +26,43 @@ function check(form)
                 return(false);
             }
 }
+
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+        showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+        showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+           slides[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+           dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        }
+
+        function clock(){
+        var refresh=1000; // Refresh rate in milli seconds
+        mytime=setTimeout('currentTime()',refresh)
+        }
+
+        function currentTime() {
+        var x = new Date()
+        var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
+        x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
+        document.getElementById('date_and_time').innerHTML = x1;
+        clock();
+        }
